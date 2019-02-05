184725ca29087a5f33d9f13635e95ea2ee9ec7d6
aadneny <aadneny@stud.ntnu.iie.no>
2019-02-05 12:31:02 +0100

    scanner

--- src/Oving1/Oving1.java
+++ src/Oving1/Oving1.java
@@ -91,13 +91,26 @@ public class Oving1 {
 
     /*
 
-    The only even prime number is 2. All other even numbers can be divided by 2.
-If the sum of a number's digits is a multiple of 3, that number can be divided by 3.
-No prime number greater than 5 ends in a 5. Any number greater than 5 that ends in a
- 5 can be divided by 5.
-Zero and 1 are not considered prime numbers.
-Except for 0 and 1, a number is either a prime number or a composite number.
-A composite number is defined as any number, greater than 1, that is not prime.
+        int[] one = {0,1,2,3,4,5,6};
+        int[] two = {7,8,9,10,11,12};
+        joinTwoArrays(one, two);
+
+
+        System.out.println("Enter amount of threads");
+        Scanner scanner = new Scanner(System.in);
+        int amountOfThreads = Integer.parseInt(scanner.nextLine());
+        System.out.println("Enter startInterval");
+        int startInterval = Integer.parseInt(scanner.nextLine());
+        System.out.println("Enter endInterval");
+        int endInterval = Integer.parseInt(scanner.nextLine());
+        System.out.println("n threads : " + amountOfThreads + "\nstart : " + startInterval + "\nend :" + endInterval );
+        Oving1 o = new Oving1();
+
+        for(int i = 0; i<amountOfThreads; i++){
+            findAllPrimesInInterval(startInterval, endInterval);
+        }
+
+
 
      */
 
