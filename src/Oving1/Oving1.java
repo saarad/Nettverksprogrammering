5c1db11b6d5cc156202e68d02fcf98743a6207bc
aadneny <aadneny@stud.ntnu.iie.no>
2019-02-05 11:19:07 +0100

    scanner

--- src/Oving1/Oving1.java
+++ src/Oving1/Oving1.java
@@ -1,7 +1,21 @@
 package Oving1;
 
+import java.util.Scanner;
+
 public class Oving1 {
+
     public static void main(String[] args){
-        System.out.println("Hello");
+        System.out.println("Enter your username: ");
+        Scanner scanner = new Scanner(System.in);
+        String username = scanner.nextLine();
+        System.out.println("Your username is " + username);
     }
 }
+
+/*
+ 1 funksjon for å sortere
+ 1 funksjon som tar imot to tall (interval) og et gitt antall tråder
+ 1 funksjon for å dele opp intervallet likt fordelt basert på antall tråder
+ 1 funksjon for å lage et gitt antall threads
+ 1 funksjon som tar imot et intervall, og finner alle primtallene i intervallet
+ */
